259c63385b4ad1ab2ec3194b25801b2cf87cf3f5
mitrakov <a.mitrakov@elama.ru>
2020-07-08 16:12:04 +0300

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,13 @@
+name := "uber-server"
+organization := "com.mitrakov.self"
+version := "1.0.0"
+scalaVersion := "2.13.1"
+
+val http4sVersion = "0.21.1"
+
+libraryDependencies ++= Seq(
+  "org.http4s"    %% "http4s-blaze-server" % http4sVersion,
+  "org.http4s"    %% "http4s-dsl"          % http4sVersion,
+  "ch.qos.logback" % "logback-classic" % "1.1.3" % Runtime,
+  "org.scalatest" %% "scalatest" % "3.2.0" % Test,
+)
---
+++ src/main/scala/com/mitrakov/self/uber/MainApp.scala
@@ -0,0 +1,37 @@
+package com.mitrakov.self.uber
+
+import cats.effect.{ExitCode, IO, IOApp}
+import org.http4s.{HttpApp, HttpRoutes}
+import org.http4s.dsl.Http4sDsl
+import org.http4s.server.Router
+import org.http4s.server.middleware.Logger
+import fs2.Stream
+import org.http4s.server.blaze.BlazeServerBuilder
+
+object MainApp extends IOApp {
+  override def run(args: List[String]): IO[ExitCode] = {
+    import cats.syntax.functor.toFunctorOps
+
+    val app: HttpApp[IO] = createApp
+    val loggedApp: HttpApp[IO] = Logger.httpApp(logHeaders = true, logBody = true)(app)
+    val server = createServer(loggedApp)
+
+    server.compile.drain.as(ExitCode.Success)
+  }
+
+  def createApp: HttpApp[IO] = {
+    val dsl = new Http4sDsl[IO] {}
+    import dsl._
+    import org.http4s.implicits._
+
+    val rootService = HttpRoutes.of[IO] {
+      case GET -> Root / "hello" => Ok(s"ok")
+    }
+
+    Router("/" -> rootService).orNotFound
+  }
+
+  def createServer(app: HttpApp[IO]): Stream[IO, ExitCode] = {
+    BlazeServerBuilder[IO].bindHttp(30001, "0.0.0.0").withHttpApp(app).serve
+  }
+}
